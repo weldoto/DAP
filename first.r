f75056151b0531840edb61e2f5e0e75b53cdb672
epa <epa@localhost.localdomain>
2017-10-04 20:49:13 +0100

    first R script

---
+++ first.r
@@ -0,0 +1,14 @@
+# correlation coefff analysis
+
+x <- c(2,7,4,1,9,3,6,8,6,4)
+
+y <- c(2,2,7,5,6,2,8,5,7,9)
+
+
+res <- cor(x,y)
+
+print(res)
+
+# correlation coefficient calculation
+#is oreder dependent and moving ther order
+# around affects  the coeffecient
